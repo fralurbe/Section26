277bb6b382338abe403a1f66df171f2c62c14995
Fran Lurbe <francisco.lurbe@gmail.com>
2022-10-13 12:30:13 +0200

    Avances

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const {check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -10,27 +11,48 @@ router.get('/signup', (req, res) => {
    res.send(signupTemplate({req}));
 });
 
-router.post('/signup', async (req, res) => {
-  console.log('app.post /signup');
-  const { email, password, passwordConfirmation } = req.body;
+router.post(
+   '/signup', 
+   [
+      check('email')
+         .trim()
+         .normalizeEmail()
+         .isEmail()
+         .withMessage('Must be a valid message')
+         .custom((email) => {
+            
+         }),
+      check('password')
+         .trim()
+         .isLength({min:4, max:16}),
+      check('passwordConfirmation')
+      .trim()
+      .isLength({min:4, max:16}),
+   ],
+   async (req, res) => {      
+      console.log('app.post /signup');
+      const errors = validationResult(req);
+      console.log(errors);
+      const { email, password, passwordConfirmation } = req.body;
 
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('Email in use');
-  }
+      const existingUser = await usersRepo.getOneBy({ email });
+      if (existingUser) {
+         return res.send('Email in use');
+      }
 
-  if (password !== passwordConfirmation) {
-    return res.send('Passwords must match');
-  }
+      if (password !== passwordConfirmation) {
+         return res.send('Passwords must match');
+      }
 
-  // Create a user in our user repo to represent this person
-  const user = await usersRepo.create({ email, password });
+      // Create a user in our user repo to represent this person
+      const user = await usersRepo.create({ email, password });
 
-  // Store the id of that user inside the users cookie
-  req.session.userId = user.id;
+      // Store the id of that user inside the users cookie
+      req.session.userId = user.id;
 
-  res.send('Account created!!!');
-});
+      res.send('Account created!!!');
+   }
+);
 
 router.get('/signout', (req, res) => {
    console.log(app.get('/signout'));
